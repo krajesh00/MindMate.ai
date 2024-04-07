0d5bb183e22bbdb665c34664c224c09962e52eaf
Aryan Kanak <akanak314@gmail.com>
2024-04-06 22:10:03 -0400

    Created concrete resource finder

---
+++ resource_finder/resources_type_enum.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class ResourcesType(Enum):
+    CONCRETE_RESOURCES = 1
+    HELPLINES = 2
+    BLOG_POSTS = 3
+    EXERCISES = 4
+    STARTING_POINT = 5
+    MOVIES = 6
+    MUSIC = 7
