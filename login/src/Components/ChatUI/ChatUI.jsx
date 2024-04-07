3cf7c764b3cfa0a1256cc8549a9db258b6b2a7ae
adi <a56mehta@uwaterloo>
2024-04-07 12:46:31 -0400

    Created ML Emotion Detection

---
+++ login/src/Components/ChatUI/ChatUI.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Nav from "../Nav/Nav.jsx"
+
+
+const ChatUI = () => {
+
+
+
+
+    return (
+        <div className='chat'>
+            <Nav />
+        </div>
+    );
+};
+
+export default ChatUI;
